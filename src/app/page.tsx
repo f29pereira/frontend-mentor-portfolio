08288241360996c8b1e95ab0087d269a6c1b443a
Filipe Pereira <f29.pereira@gmail.com>
2025-09-15 09:36:15 +0100

    Update Card and LinkButton styling, Add new Card for Fylo challenge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,24 +3,32 @@ import Card from "./components/Card/Card";
 
 export default function Home() {
   return (
-    <main className="mt-20 p-4">
+    <main className="mt-10 p-4 lg:mt-20">
       <div className="flex justify-center">
-        <div className="grid grid-cols-1 gap-8 lg:grid-cols-2">
+        <div className="grid grid-cols-1 gap-16 lg:grid-cols-2 xl:grid-cols-3 xl:gap-8">
           <Card
-            imageSrc="/images/order_summary.png"
-            imageAlt="Image of Order Summary Challenge"
-            title="Order Summary"
+            imageSrc="/images/fylo_landing_page.png"
+            imageAlt="Fylo Landing Page Challenge"
+            title="Fylo landing page"
             desctiption="Built with: Next.js, React, TypeScript, HTML and CSS"
-            demoLink="https://f29pereira.github.io/order-summary/"
-            codeLink="https://github.com/f29pereira/order-summary"
+            demoLink="https://f29pereira.github.io/fylo/"
+            codeLink="https://github.com/f29pereira/fylo"
           />
           <Card
             imageSrc="/images/bento_grid.png"
-            imageAlt="Image of Bento Grid Challenge"
+            imageAlt="Bento Grid Challenge"
             title="Bento Grid"
             desctiption="Built with: Next.js, React, TypeScript, HTML and CSS"
             demoLink="https://f29pereira.github.io/bento-grid/"
             codeLink="https://github.com/f29pereira/bento-grid"
+          />
+          <Card
+            imageSrc="/images/order_summary.png"
+            imageAlt="Order Summary Challenge"
+            title="Order Summary"
+            desctiption="Built with: Next.js, React, TypeScript, HTML and CSS"
+            demoLink="https://f29pereira.github.io/order-summary/"
+            codeLink="https://github.com/f29pereira/order-summary"
           />
         </div>
       </div>
