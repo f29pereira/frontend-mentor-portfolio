a4c8a994cdfa1bbe8b87c9a438fdaddf268ba648
Filipe Pereira <f29.pereira@gmail.com>
2026-05-15 21:36:59 +0100

    refactor: Footer component styles with clsx

--- src/app/components/sections/Footer/Footer.tsx
+++ src/app/components/sections/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import clsx from "clsx";
 import {
   FaLinkedin,
   SiGmail,
@@ -10,19 +11,23 @@ import {
  */
 export default function Footer() {
   return (
-    <footer className="bg-blue-800 min-h-20 text-white p-12 lg:p-20">
+    <footer className={clsx("bg-blue-800 min-h-20 text-white p-12", "lg:p-20")}>
       <div
-        className="flex flex-col items-center gap-12 
-                      lg:flex-row lg:justify-center lg:gap-20"
+        className={clsx(
+          "flex flex-col items-center gap-12",
+          "lg:flex-row lg:justify-center lg:gap-20",
+        )}
       >
         {/*Email*/}
-        <div className="flex justify-center items-center gap-4">
+        <div className={clsx("flex justify-center items-center gap-4")}>
           <SiGmail className="text-xl" />
-          <span className="text-sm lg:text-base">f29.pereira@gmail.com</span>
+          <span className={clsx("text-sm lg:text-base")}>
+            f29.pereira@gmail.com
+          </span>
         </div>
 
         {/*Account links*/}
-        <div className="flex flex-row gap-12 text-4xl lg:gap-20">
+        <div className={clsx("flex flex-row gap-12 text-4xl", "lg:gap-20")}>
           <a
             href="https://www.linkedin.com/in/filipe-pereira-b757471b9"
             target="_blank"
