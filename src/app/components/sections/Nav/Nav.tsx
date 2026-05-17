0d8a8ef1db9d1718c56a66fe5b164cb5ea154646
Filipe Pereira <f29.pereira@gmail.com>
2026-05-17 11:47:54 +0100

    feat: update Nav component styling
    - Remove code icon
    - Add margin, padding and text size for different screen sizes

--- src/app/components/sections/Nav/Nav.tsx
+++ src/app/components/sections/Nav/Nav.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import styles from "./Nav.module.css";
-import { FaCode } from "@/app/utils/icons";
 
 /**
  * Renders Navigation Bar
@@ -9,17 +8,25 @@ export default function Nav() {
   return (
     <nav
       className={clsx(
+        "px-4 pt-8",
         styles.nav,
-        "flex justify-center items-center h-26 bg-blue-800",
+        "sm:px-10",
+        "md:px-12 md:pt-12",
+        "lg:px-14 xl:px-16 2xl:px-20",
+        "3xl:px-40",
       )}
     >
-      <ul className={clsx("text-center text-white")}>
-        <div className={clsx("flex justify-center items-center gap-4")}>
-          <FaCode className={clsx("text-3xl", "sm:text-4xl")} />
-          <li className={clsx("text-xl", "sm:text-2xl", "lg:text-3xl")}>
-            Frontend Mentor Challenges
-          </li>
-        </div>
+      <ul className={clsx("border-b-2 border-stone-300 text-center")}>
+        <li
+          className={clsx(
+            "mb-8 text-2xl text-blue-800 leading-10 tracking-wider",
+            "sm:text-3xl",
+            "md:mb-12",
+            "lg:text-4xl",
+          )}
+        >
+          Filipe Pereira - Frontend Mentor Challenges
+        </li>
       </ul>
     </nav>
   );
