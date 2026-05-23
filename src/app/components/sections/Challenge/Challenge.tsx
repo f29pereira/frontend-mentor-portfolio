0095f504dcf4599b4dddbce0cb2d14f4a636a81e
Filipe Pereira <f29.pereira@gmail.com>
2026-05-23 12:19:40 +0100

    feat: add dark theme styling and update description off the Challenge component
    - Update globals.css file with dark theme custom style for inline links

--- src/app/components/sections/Challenge/Challenge.tsx
+++ src/app/components/sections/Challenge/Challenge.tsx
@@ -15,24 +15,29 @@ export default function Challenge() {
         className={clsx(
           "mb-4 text-2xl font-bold text-indigo-950",
           "lg:text-3xl xl:mb-8",
+          "dark:text-dark-title",
         )}
       >
         Challenges
       </h2>
-
       {/*Description*/}
-      <p>
+      <p className={clsx("mb-2", "dark:text-dark-text")}>
         This is a collection of coding challenges I&apos;ve completed from{" "}
         <a
           className={clsx(
             "underline text-blue-950 hover:text-cyan-500",
             "after:content-['_↗']",
+            "dark:text-dark-inline-link",
           )}
           href="https://www.frontendmentor.io/home"
           target="_blank"
         >
           Frontend Mentor
         </a>
+        . In these challenges I focused on building{" "}
+        <strong className="dark:font-extrabold">mobile-first</strong>,{" "}
+        <strong className="dark:font-extrabold">accessible</strong> and{" "}
+        <strong className="dark:font-extrabold">responsive layouts</strong>.
       </p>
 
       {/*List of challenges*/}
