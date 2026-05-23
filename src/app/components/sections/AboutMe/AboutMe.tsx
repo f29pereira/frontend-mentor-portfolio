535d633ffe870ce600d49137a2c2d6ca8a8f36c6
Filipe Pereira <f29.pereira@gmail.com>
2026-05-23 11:35:35 +0100

    feat: add dark theme styling to the AboutMe component
    - Add dark theme styles for the main title, text and "Download CV" link
    - Increase top margin for the "Download CV" link container

--- src/app/components/sections/AboutMe/AboutMe.tsx
+++ src/app/components/sections/AboutMe/AboutMe.tsx
@@ -15,40 +15,43 @@ export default function AboutMe() {
         className={clsx(
           "mb-4 text-3xl font-bold text-indigo-950",
           "lg:text-4xl xl:mb-8",
+          "dark:text-dark-title",
         )}
       >
         About me
       </h1>
 
       {/*Text*/}
-      <p className="mb-2">
+      <p className={clsx("mb-2", "dark:text-dark-text")}>
         Hello, my name is Filipe Pereira and I&apos;m a{" "}
-        <strong>software developer</strong> with{" "}
-        <strong>3 years of experience</strong>. I had the opportunity to develop
-        and maintain various applications using JavaScript, Spring Framework,
-        SQL, and other technologies.
+        <strong className="dark:font-extrabold">software developer</strong> with{" "}
+        <strong className="dark:font-extrabold">3 years of experience</strong>.
+        I had the opportunity to develop and maintain various applications using
+        JavaScript, Spring Framework, SQL, and other technologies.
       </p>
-      <p className="mb-2">
-        During my <strong>internship</strong>, I worked as a{" "}
-        <strong>full-stack</strong> developer using the Laravel Framework, HTML,
-        CSS, Bootstrap, Adminlte and phpMyAdmin.
+      <p className={clsx("mb-2", "dark:text-dark-text")}>
+        During my <strong className="dark:font-extrabold">internship</strong>, I
+        worked as a <strong className="dark:font-extrabold">full-stack</strong>{" "}
+        developer using the Laravel Framework, HTML, CSS, Bootstrap, Adminlte
+        and phpMyAdmin.
       </p>
-      <p className="mb-2">
-        I value <strong>teamwork</strong> and continuous learning, always
-        motivated to share knowledge.
+      <p className={clsx("mb-2", "dark:text-dark-text")}>
+        I value <strong className="dark:font-extrabold">teamwork</strong> and
+        continuous learning, always motivated to share knowledge.
       </p>
-      <p>
+      <p className={clsx("dark:text-dark-text")}>
         Currently, my main goal is to specialize in{" "}
-        <strong>web development</strong>, focusing on React. I recently obtained
-        certifications in React, React Router, Next.js, TypeScript, and Tailwind
-        CSS, and I&apos;ve been developing personal projects to consolidate and
-        apply the knowledge I&apos;ve acquired.
+        <strong className="dark:font-extrabold">web development</strong>,
+        focusing on React. I recently obtained certifications in React, React
+        Router, Next.js, TypeScript, and Tailwind CSS, and I&apos;ve been
+        developing personal projects to consolidate and apply the knowledge
+        I&apos;ve acquired.
       </p>
 
       {/*Download CV link*/}
       <div
         className={clsx(
-          "flex justify-center items-center mt-8",
+          "flex justify-center items-center mt-10",
           "sm:mt-12",
           "xl:mt-16",
           "3xl:mt-20",
@@ -61,6 +64,7 @@ export default function AboutMe() {
             "text-center text-white font-bold",
             "cursor-pointer rounded-full",
             "md:px-10 md:py-6 md:hover:bg-indigo-500",
+            "dark:bg-indigo-500 dark:shadow-none dark:hover:bg-indigo-400",
           )}
           aria-label="Dowload my CV as PDF"
           href="cv/CV_Filipe_Pereira.pdf"
