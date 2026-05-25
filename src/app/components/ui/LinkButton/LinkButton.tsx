f5cc0b0db368046199ba82d24b5e97192487b869
Filipe Pereira <f29.pereira@gmail.com>
2026-05-25 13:45:22 +0100

    feat: update style of the LinkButton component
    - update to a anchor tag instead of Next.js Link component (Link should be used for routes)
    - update the border to be like a pill button
    - Add font fize for small screens (640px)
    - Add gap between elements of the link description for medium screens (768px)

--- src/app/components/ui/LinkButton/LinkButton.tsx
+++ src/app/components/ui/LinkButton/LinkButton.tsx
@@ -1,22 +1,31 @@
+import clsx from "clsx";
 import type { LinkButtonProps } from "@/app/types";
-import Link from "next/link";
 
 /**
- * Renders Link component styled like a button
- * Always opens link in a new tab.
+ * Renders a link styled like a pill button
+ * Always opens the link in a new tab.
  *
  * Props are defined in {@link LinkButtonProps}.
  */
 export default function LinkButton({ link, description }: LinkButtonProps) {
   return (
-    <Link
-      className="bg-indigo-600 py-3 px-6 rounded-xl text-sm hover:bg-indigo-500 md:text-base "
+    <a
+      className={clsx(
+        "py-3 px-6 rounded-full text-sm bg-indigo-600",
+        "hover:bg-indigo-500",
+        "sm:text-base",
+      )}
       target="_blank"
       href={link}
     >
-      <span className="flex justify-center items-center gap-2 text-white">
+      <span
+        className={clsx(
+          "flex justify-center items-center gap-2 text-white",
+          "md:gap-4",
+        )}
+      >
         {description}
       </span>
-    </Link>
+    </a>
   );
 }
