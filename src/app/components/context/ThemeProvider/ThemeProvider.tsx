302b08c9c3e51bd42a233504dbde3eb72afa9c9e
Filipe Pereira <f29.pereira@gmail.com>
2026-05-27 15:53:19 +0100

    feat: update ThemeProvider to set the dark theme as default

--- src/app/components/context/ThemeProvider/ThemeProvider.tsx
+++ src/app/components/context/ThemeProvider/ThemeProvider.tsx
@@ -10,7 +10,7 @@ export const ThemeContext = createContext<ThemeContextType | undefined>(
  * Provides the context ThemeContext to be able to toggle the app theme
  */
 export default function ThemeProvider({ children }: ReactChildrenProp) {
-  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [isDarkTheme, setIsDarkTheme] = useState(true);
 
   /**
    * Toggles the class dark to the html tag
