c344513752bd1a94269ceb9dacf18110955376da
Filipe Pereira <f29.pereira@gmail.com>
2026-05-28 22:02:13 +0100

    feat: update ThemeProvider with localstorage for the app theme
    - Update index.ts type for the ThemeContext: allow the property isDarkTheme to accept null value

--- src/app/components/context/ThemeProvider/ThemeProvider.tsx
+++ src/app/components/context/ThemeProvider/ThemeProvider.tsx
@@ -6,18 +6,50 @@ export const ThemeContext = createContext<ThemeContextType | undefined>(
   undefined,
 );
 
+enum Theme {
+  Light = "light",
+  Dark = "dark",
+}
+
 /**
  * Provides the context ThemeContext to be able to toggle the app theme
  */
 export default function ThemeProvider({ children }: ReactChildrenProp) {
-  const [isDarkTheme, setIsDarkTheme] = useState(true);
+  const [isDarkTheme, setIsDarkTheme] = useState<boolean | null>(null);
 
   /**
-   * Toggles the class dark to the html tag
+   * Creates localstorage for the dark theme if no localstorage is present
    */
   useEffect(() => {
+    const appTheme = localStorage.getItem("app_theme");
+
+    if (appTheme) {
+      setIsDarkTheme(appTheme === Theme.Dark);
+    } else {
+      // Default theme
+      localStorage.setItem("app_theme", Theme.Dark);
+      setIsDarkTheme(true);
+    }
+  }, []);
+
+  /**
+   * Toggles the class dark to the html tag and updates the localstorage
+   */
+  useEffect(() => {
+    if (isDarkTheme === null) {
+      return;
+    }
     document.documentElement.classList.toggle("dark", isDarkTheme);
+    localStorage.setItem("app_theme", getTheme(isDarkTheme));
   }, [isDarkTheme]);
+
+  /**
+   * Returns the string corresponding to the app theme
+   * @param isDarkTheme isDarkTheme state
+   */
+  const getTheme = (isDarkTheme: boolean): string => {
+    return isDarkTheme ? Theme.Dark : Theme.Light;
+  };
 
   /**
    * Toggles the dark theme
@@ -27,7 +59,9 @@ export default function ThemeProvider({ children }: ReactChildrenProp) {
   };
 
   return (
-    <ThemeContext value={{ isDarkTheme: isDarkTheme, toggle: toggleDarkTheme }}>
+    <ThemeContext
+      value={{ isDarkTheme: isDarkTheme ?? true, toggle: toggleDarkTheme }}
+    >
       {children}
     </ThemeContext>
   );
